3245f670913a5262d535db319cf526442cb2f69b
Adrián <jasmdp@hotmail.com>
2022-09-07 19:27:33 -0300

    Contacto form controls, done

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,5 +1,25 @@
+import { Layout } from "../../components";
+
 const Contact = () => {
-  return <div>Formulario de contacto</div>;
+  return (
+    <Layout>
+      <form action="">
+        <div>
+          <label htmlFor="">Nombre</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="">Mensaje</label>
+          <textarea name="mensaje"></textarea>
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </Layout>
+  );
 };
 
 export { Contact };
